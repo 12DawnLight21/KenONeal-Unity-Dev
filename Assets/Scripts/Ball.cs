1be17fb2b8567e1ff8e07f511581bef142df7ea2
Kenneth ONeal <12keniaoneal21@gmail.com>
2024-01-11 18:23:08 -0700

    1/11/24 Caught up on what I missed - Ken

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [Header("Movement")]
-    //[SerializedField][Range(1, 20)][Tooltip("Force required to move object")]float force;
-    public float force;
+	[Header("Movement")]
+	[SerializeField][Range(1, 20)][Tooltip("Force required to move object")] float force;
 
+	public Rigidbody rb;
 
-    public Rigidbody rb;
+	private void Awake() // when the models are loaded in
+	{
+		//print("awake");
+	}
 
-    private void Awake() // when the models are loaded in
-    {
-        //print("awake");
-    }
+	void Start() // when every component can be used
+	{
+		//print("start");
+		rb = GetComponent<Rigidbody>();
+	}
 
-    // Start is called before the first frame update
-    void Start() // when every component can be used
-    {
-        //print("start");
-        //rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-           rb.AddForce(transform.up * force, ForceMode.VelocityChange);
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(transform.up * force, ForceMode.VelocityChange);
+		}
+	}
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,20 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    [SerializeField][Range(-360, 360)] float rate;
+	[SerializeField][Range(-360, 360)] float angle;
 
-    float speed = 5;
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.rotation *= Quaternion.AngleAxis(rate * Time.deltaTime, transform.up);
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
-    }
+	void Update()
+	{
+		transform.rotation *= Quaternion.AngleAxis(angle * Time.deltaTime, Vector3.up);
+	}
 }
